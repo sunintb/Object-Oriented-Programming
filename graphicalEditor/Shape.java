a8dbb21447fb73f1956788b733585be054a0eb71
sunintb <66996976+sunintb@users.noreply.github.com>
2020-06-18 22:40:53 -0600

    Update Shape.java

--- graphicalEditor/Shape.java
+++ graphicalEditor/Shape.java
@@ -4,8 +4,6 @@ import java.awt.Graphics;
 /**
  * A geometric entity with a color
  * 
- * @author Chris Bailey-Kellogg, Dartmouth CS 10, Spring 2016, based on a related concept from previous terms
- * @author CBK, revised Fall 2016
  */
 
 public interface Shape {
